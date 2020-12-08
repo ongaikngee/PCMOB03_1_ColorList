d4bc58f3d3ddfe6d1268f0b856e7f138a858d1cf
Glippy <jonathanong@iMac.local>
2020-12-08 15:44:40 +0800

    Added Grid layout

--- App.js
+++ App.js
@@ -1,22 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState, useEffect } from "react";
-import { Button, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { Button, StyleSheet, Text, View, TouchableOpacity, Dimensions } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import BlockRGB from './components/BlockRGB';
 import { FlatList } from 'react-native-gesture-handler';
 
 function HomeScreen({ navigation }) {
+
+	const numColumns = 5;
+
 	const [ colorArray, setColorArray ] = useState([
-		{ red: 255, green: 0, blue: 0, id: '0' },
-		{ red: 0, green: 255, blue: 0, id: '1' },
-		{ red: 0, green: 0, blue: 255, id: '2' }
+		{ red: 255, green: 255, blue: 0, col:numColumns, id: '0' },
+		// { red: 0, green: 255, blue: 0, id: '1' },
+		// { red: 0, green: 0, blue: 255, id: '2' }
 	]);
 
 	function renderItem({ item }) {
 		return (
 			<TouchableOpacity onPress={() => navigation.navigate('Details', { ...item })}>
-				<BlockRGB red={item.red} green={item.green} blue={item.blue} />
+				<BlockRGB red={item.red} green={item.green} blue={item.blue} col={item.col}/>
 			</TouchableOpacity>
 		);
 	}
@@ -29,6 +32,7 @@ function HomeScreen({ navigation }) {
 				red: Math.floor(Math.random() * 255),
 				green: Math.floor(Math.random() * 255),
 				blue: Math.floor(Math.random() * 255),
+				col: numColumns,
 				id: `${colorArray.length}`
 				//you could explore UUID
 				//you could use time in the moment
@@ -54,7 +58,7 @@ function HomeScreen({ navigation }) {
 				<Button title="Reset" onPress={reset} /> */}
 				{/* <Button title="Details" onPress={() => navigation.navigate("DetailsScreen", { ...item })} /> */}
 			</View>
-			<FlatList contentContainerStyle={styles.list} data={colorArray} renderItem={renderItem} />
+			<FlatList contentContainerStyle={styles.list} data={colorArray} renderItem={renderItem} numColumns={numColumns} />
 		</View>
 	);
 }
@@ -93,9 +97,6 @@ const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 		backgroundColor: '#fff'
-		// alignItems: 'center',
-		// justifyContent: 'center',
-		// width:"100%",
 	},
 	buttonContainer: {
 		display: 'flex',
--- components/BlockRGB.js
+++ components/BlockRGB.js
@@ -1,23 +1,27 @@
 import React from 'react';
-import {Text, View, StyleSheet} from 'react-native';
+import { Text, View, StyleSheet, Dimensions } from 'react-native';
 
-export default function BlockRGB(props){
-    return(
-        <View
-        style={{
-          backgroundColor: `rgb(${props.red}, ${props.green}, ${props.blue})`,
-          padding: 30,
-          width: "100%",
-        }}
-      ></View>
-   
-    );
+export default function BlockRGB(props) {
+  // console.log(props);
 
+	const screenWidth = Dimensions.get('window').width;
+  const numColumns = props.col;
+	const tileSize = screenWidth / numColumns;
 
+	return (
+		<View
+			style={{
+				backgroundColor: `rgb(${props.red}, ${props.green}, ${props.blue})`,
+				padding: 30,
+				width: tileSize,
+        height: tileSize,
+        // height: 100,
+        // flex:1,
+        // aspectRatio:1,
+        // flex:1/numColumns,
+			}}
+		/>
+	);
 }
 
-const styles = StyleSheet.create({
-
-    
-    
-});
+const styles = StyleSheet.create({});
